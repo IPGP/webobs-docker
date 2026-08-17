3921d712fbdeaa9af378d6401a477c57f988b081
PBoissier <boissier@ipgp.fr>
2026-08-17 09:58:45 +0400

    Update Dockerfile to use latest WebObs distribution
    Also fix automatic password generation.

--- webobs/docker-entrypoint.sh
+++ webobs/docker-entrypoint.sh
@@ -4,7 +4,12 @@ set -euo pipefail
 STATE_DIR="/var/lib/webobs-docker"
 MARKER="$STATE_DIR/.initialized"
 mkdir -p "$STATE_DIR"
-WO_USER=$(stat -c '%U' ${INSTALL_DIR}/CONF)
+
+if [ -d "${INSTALL_DIR}/CONF" ]; then
+    WO_USER=$(stat -c '%U' "${INSTALL_DIR}/CONF")
+else
+    WO_USER="wo"
+fi
 
 set_password() {
     local user="$1" env_var="$2"
@@ -17,13 +22,22 @@ set_password() {
         password="${!env_var}"
     elif [[ ! -f "$MARKER" ]]; then
         password="$(openssl rand -base64 18)"
-        echo "[entrypoint] Password generated for '${user}' : ${password}"
-        echo "[entrypoint] Store it securely, it will not be displayed again."
+    	OUTFILE="${INSTALL_DIR}/CONF/.wo_generated_password"
+		echo "${password}" > "${OUTFILE}"
+		chmod 600 "${OUTFILE}"
+		chown "${WO_USER}:${WO_USER}" "${OUTFILE}" 2>/dev/null || true
+		echo "**** No WO_USERPASSWD[_FILE] provided: a random password was generated"
+		echo "**** for WebObs user '${WO_USER}' and written to:"
+		echo "****     ${OUTFILE}  (on the host, if that directory is a mounted volume)"
+		echo "**** Retrieve it with e.g.: docker exec <container> cat ${OUTFILE}"
     fi
 
     if [[ -n "$password" ]]; then
         echo "${user}:${password}" | chpasswd
     fi
+
+    #TODO : add htpasswd command for wo (check if htpasswd file exists, if not create it)
+
 }
 
 if [[ ! -f "$MARKER" ]]; then
